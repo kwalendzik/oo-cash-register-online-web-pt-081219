e04752edb923ddcb5fb2eb3636f619b73b3ffedf
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-28 19:25:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ def add_item(title, price, quantity = 1)
   quantity.times do
   items << title
   end
-  @last_transaction = price*quantity
+  @last_transaction << price*quantity
 end
 
 def apply_discount

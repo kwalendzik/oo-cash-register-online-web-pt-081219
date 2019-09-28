4a199905e7019df39f3e92ab7c0363a305d29096
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-28 19:07:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,17 @@
 require 'pry'
 
 class CashRegister
-attr_accessor :total, :item, :discount, :void 
+attr_accessor :total, :discount, :items, :void 
   
 def initialize(discount = 0)
  @total = 0
  @discount = discount
+ @items = []
+ @void = []
 end 
   
-  
+def discount
+  self.discount
+end
   
 end
